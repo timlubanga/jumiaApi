74c0a0285bd9e93e3ad2d8107cc10bd51b2596fc
timlubanga <timlubanga@gmail.com>
2021-03-31 18:28:14 +0300

    configure cor-headers

--- Partners/views.py
+++ Partners/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+
+from Partners.models import Supplier, Brand
+from Partners.serializers import SupplierSerializer, BrandSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
+
+
+class SupplierViewset(viewsets.ModelViewSet):
+    serializer_class = SupplierSerializer
+    permission_classes = []
+    queryset = Supplier.objects.all()
+
+
+class BrandViewset(viewsets.ModelViewSet):
+    serializer_class = BrandSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Brand.objects.all()
